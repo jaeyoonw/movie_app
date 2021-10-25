0a5f4018a6330951c2dee5bbcbf3b273879afdb8
jaeyoonw <jaeyoon.dev@gmail.com>
2021-10-25 18:06:06 +0900

    Component life cycle and state

--- src/App.js
+++ src/App.js
@@ -1,45 +1,20 @@
  import React from 'react';
  import PropTypes from 'prop-types'
 
- const foodLike = [
-   {
-     id: 1,
-     name: "Kimchi",
-     image: "blablabla",
-     rating: "string"
-   },
-   {
-     id: 2,
-     name: "Pizza",
-     image: "blablabla2",
-     rating: 5.0
-   }
- ];
-
-function Food({name, picture, rating}) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <img src={picture} alt={name}></img>
-      <h4>{rating}/5.0</h4>
-    </div>
-  )
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false })
+    }, 6000);
+  }
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>
+  }
 }
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-}
-
-function App() {     // 이게 App Component
-  return ( 
-    <div>
-      <h1>Hello React!</h1>
-      {foodLike.map(food => (
-        <Food key={food.id} name={food.name} picture={food.image} rating={food.rating} />
-      ))}
-    </div>
-  )
-}
 export default App;
